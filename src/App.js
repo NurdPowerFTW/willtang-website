170891f7f863c6354ce3837a915fe1df3074525a
William Tang <u1169036@utah.edu>
2019-01-29 20:32:32 -0700

    Make a static http reference to resumeData.json

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'./resumeData.json',
+      url:'https://raw.githubusercontent.com/NurdPowerFTW/willtang-website/gh-pages/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
