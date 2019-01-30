28b1fb1d5deb1d58c0270794ea185e86f17fa79b
William Tang <u1169036@utah.edu>
2019-01-29 20:18:37 -0700

    Updates

---
+++ precache-manifest.015600dd38dc8620195eb41b5e408387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0de964bf56756c9baa7",
+    "url": "/willtang-website/static/css/main.2d4dfd2c.chunk.css"
+  },
+  {
+    "revision": "d0de964bf56756c9baa7",
+    "url": "/willtang-website/static/js/main.d0de964b.chunk.js"
+  },
+  {
+    "revision": "7c18a8a23e4d84adb5c1",
+    "url": "/willtang-website/static/js/1.7c18a8a2.chunk.js"
+  },
+  {
+    "revision": "f1a9c7bdb2c9c9644627",
+    "url": "/willtang-website/static/js/runtime~main.f1a9c7bd.js"
+  },
+  {
+    "revision": "7072de21b6948468ce72b5f45fa8c6f8",
+    "url": "/willtang-website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/willtang-website/precache-manifest.015600dd38dc8620195eb41b5e408387.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/willtang-website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
